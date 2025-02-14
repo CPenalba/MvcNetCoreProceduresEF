0e7eacd3eb9793c5da1dd12c3adb918dfb6f5151
CPenalba <carolinapenalbacorpa@gmail.com>
2025-02-14 10:03:33 +0100

    Vista empleado

--- MvcNetCoreProceduresEF/Program.cs
+++ MvcNetCoreProceduresEF/Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddControllersWithViews();
 string connectionString = builder.Configuration.GetConnectionString("SqlHospital");
 builder.Services.AddTransient<RepositoryEnfermos>();
 builder.Services.AddTransient<RepositoryDoctores>();
-builder.Services.AddDbContext<EnfermosContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddTransient<RepositoryEmpleados>();
+builder.Services.AddDbContext<HospitalContext>(options => options.UseSqlServer(connectionString));
 
 
 var app = builder.Build();
---
+++ MvcNetCoreProceduresEF/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using MvcNetCoreProceduresEF.Data;
+using MvcNetCoreProceduresEF.Models;
+
+#region
+//CREATE VIEW V_EMPLEADOS_DEPARTAMENTOS
+//AS
+//SELECT CAST(ISNULL(ROW_NUMBER() OVER (ORDER BY APELLIDO), 0) AS int) as ID,
+//EMP.APELLIDO, EMP.OFICIO, EMP.SALARIO, DEPT.DNOMBRE AS DEPARTAMENTO,
+//DEPT.LOC AS LOCALIDAD FROM EMP
+//INNER JOIN DEPT ON EMP.DEPT_NO = DEPT.DEPT_NO
+//GO
+#endregion
+namespace MvcNetCoreProceduresEF.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private HospitalContext context;
+
+        public RepositoryEmpleados(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<VistaEmpleado>> GetVistaEmpleadosAsync()
+        {
+            var consulta = from datos in this.context.VistasEmpleados select datos;
+            return await consulta.ToListAsync();
+        }
+    }
+}
